ae45208d9fd94cae4f728d6a750ce689b07bd3a5
N-ckM <njmills@millersville.edu>
2024-10-26 14:30:07 -0400

    Refined FOV
    Improved FOV ability by adding two rays stemming from the player to the edge of the vision circle, showing the range of the intended vision cone (which can be widened or squished with the view radius). can rotate player on z axis, and fov changes as it should poggers

--- TeamSkeletonStealthGame/Assets/Scripts/FieldOfView.cs
+++ TeamSkeletonStealthGame/Assets/Scripts/FieldOfView.cs
@@ -11,9 +11,13 @@ public class FieldOfView : MonoBehaviour
 {
 
     public float viewRadius;
+    [Range(0,360)]
     public float viewAngle;
 
-    public Vector3 DirFromAngle(float angleInDegrees){
-        return new Vector3(Mathf.Sin(angleInDegrees*Mathf.Deg2Rad),0, Mathf.Cos(angleInDegrees*Mathf.Deg2Rad));
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal){
+        if(!angleIsGlobal){
+            angleInDegrees += transform.eulerAngles.z;
+        }
+        return new Vector3(Mathf.Cos(angleInDegrees*Mathf.Deg2Rad), Mathf.Sin(angleInDegrees*Mathf.Deg2Rad),0);
     }
 }
