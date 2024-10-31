5fee2c70b600c59246e868acae1e66dc19b09ef3
N-ckM <njmills@millersville.edu>
2024-10-30 21:59:18 -0400

    Revert "Merge branch 'main' of https://github.com/mucsci-students/2024fa-475-Skeleton"
    This reverts commit cc2288d764fc30b1641157c54658c2ed9a5cdec5, reversing
    changes made to 1fcc8c12a334d46bb31d04a81cab6c7270d5e43e.

--- TeamSkeletonStealthGame/Assets/Scripts/PlayerMoves.cs
+++ TeamSkeletonStealthGame/Assets/Scripts/PlayerMoves.cs
@@ -4,17 +4,14 @@ using UnityEngine.Events;
 [RequireComponent(typeof(Player))]
 public class PlayerMoves : Player
 {
-    public float normalSpeed = 5.0f;
-    public float stealthSpeed = 2.5f;
-    private bool isMoving;
-
+    public float normalSpeed = 3.0f;
+    public float stealthSpeed = 1.5f;
 
     private void Start()
     {
         // Initialize Player components
         rb = GetComponent<Rigidbody2D>();
         movement = GetComponent<Animator>();
-        FOV = GetComponent<FieldOfView>();
 
         audio_source = GetComponent<AudioSource>();
         onDeath += HandlePlayerDeath;
@@ -56,11 +53,11 @@ public class PlayerMoves : Player
             Punch();
 
         if (Input.GetKeyDown(KeyCode.Space))
-            ///Jumpkick();
+            Jumpkick();
 
         if (Input.GetKeyDown(KeyCode.K))
             TakeDamage(20);
-            //self inflict damage for testing only. 
+
         if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
             ToggleStealth();
         
@@ -73,34 +70,29 @@ public class PlayerMoves : Player
 
         rb.velocity = movementVector;
 
-        isMoving = movementVector.magnitude > 0.01f;
+        bool isMoving = movementVector.magnitude > 0.01f;
         movement.SetBool("isMoving", isMoving);
 
-        if (isMoving) 
-        {
-            if (moveX < 0) 
-            {
-                transform.localScale = new Vector3(-0.8f, 0.8f, 0.8f); // Face left
-            } 
-            else if (moveX > 0) 
-            {
-                transform.localScale = new Vector3(0.8f, 0.8f, 0.8f); // Face right
+        if (isMoving && moveX != 0) {
+            transform.localScale = new Vector3(Mathf.Sign(moveX) * 0.8f, 0.8f, 0.8f);
+            if (moveX > 0) {
+                this.GetComponent<FieldOfView>().fovRotation = 85f;
+                this.GetComponent<FieldOfView>().DrawFieldofView();
+            } else {
+                this.GetComponent<FieldOfView>().fovRotation = 265f;
+                this.GetComponent<FieldOfView>().DrawFieldofView();
             }
-
-            FOV.fovRotation = (moveX > 0) ? 85f : 265f;
-            FOV.DrawFieldofView();
         }
-
     }
 
-    /**private void Jumpkick()
+    private void Jumpkick()
     {
         if (Mathf.Abs(rb.velocity.y) < 0.001f)
         {
             rb.AddForce(Vector2.up * 2, ForceMode2D.Impulse);
             movement.SetTrigger("Jumpkick");
         }
-    }*/
+    }
 
     private void Punch()
     {
